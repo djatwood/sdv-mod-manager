6058577afc7035345de37e5cd1ecc708132e946f
Daniel Atwood <daniel@atwood.io>
2020-12-27 02:23:20 -0500

    Added database functionality

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
+pub use polodb_core::db::Database;
+pub use polodb_core::db::DbResult;
 use std::env;
 use std::fs;
 
 fn main() {
+    let mut db = match Database::open("db.json") {
+        Err(e) => panic!("Failed to connect to database: {}", e),
+        Ok(db) => db,
+    };
+    let mut store = match db.collection("mods") {
+        Err(e) => panic!("Failed to get collection mods: {}", e),
+        Ok(store) => store,
+    };
+
     let args: Vec<String> = env::args().collect();
 
     match args[1].as_str() {
         "import" => import(&args[2]),
         "mods" => {
-            if args.len() < 3 {
-                list_mods(&String::new());
+            let status = if args.len() > 2 {
+                String::from(&args[2])
             } else {
-                list_mods(&args[2]);
-            }
+                String::new()
+            };
+            list_mods(&status);
         }
         _ => println!("TODO: HELP MENU"),
     }
