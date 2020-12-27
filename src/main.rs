ddcc1093fa04a00bbb083a9bb642765b2c311d8d
Daniel Atwood <daniel@atwood.io>
2020-12-27 01:06:21 -0500

    Setup inital command matching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, World!")
+    let args: Vec<String> = env::args().collect();
+
+    match args[1].as_str() {
+        "import" => import(&args[2]),
+        "mods" => {
+            if args.len() < 3 {
+                list_mods(&String::new());
+            } else {
+                list_mods(&args[2]);
+            }
+        }
+        _ => println!("TODO: HELP MENU"),
+    }
+
+    let paths = fs::read_dir("../Mods").unwrap();
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display())
+    }
+}
+
+fn list_mods(kind: &String) {
+    match kind.as_str() {
+        "enabled" => println!("list enabled mods"),
+        "disabled" => println!("list disabled mods"),
+        "" => println!("list all mods"),
+        _ => println!("TODO: show options"),
+    }
+}
+
+fn import(md: &String) {
+    println!("import {}", md)
 }
